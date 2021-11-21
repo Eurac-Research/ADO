16ee503a76e8e6c38379db089754742b9f75e62d
tiacop <thomas.iacopino@eurac.edu>
2021-11-21 09:12:34 +0100

    Update [slug].js

--- ado/pages/[slug].js
+++ ado/pages/[slug].js
@@ -352,8 +352,8 @@ export default function App( { datatype, staticData, staticMetaData, href } ) {
           </div>
         </div>
 
-        <div className="darkModeToggle" onClick={toggleTheme} title={theme === 'dark' ? 'dark mode' : "light mode"}>
-          {theme === 'dark' ? 
+        <div className="darkModeToggle" onClick={toggleTheme} title={theme === 'light' ? 'switch to dark mode' : "switch to light mode"}>
+          {theme === 'light' ? 
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" title="moon">
               <path d="M283.21 512c78.96 0 151.08-35.92 198.86-94.8 7.07-8.7-.64-21.42-11.56-19.34-124.2 23.65-238.27-71.58-238.27-196.96a200.43 200.43 0 0 1 101.5-174.39C343.43 21 341 6.31 330 4.28A258.16 258.16 0 0 0 283.2 0c-141.3 0-256 114.51-256 256 0 141.3 114.51 256 256 256z"/>
             </svg>
