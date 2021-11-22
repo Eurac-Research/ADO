ebdaf7fcd806476c58290df524ba72ab9f7a2757
tiacop <thomas.iacopino@eurac.edu>
2021-11-22 09:13:13 +0100

    add google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { GA_TRACKING_ID } from '@/lib/gtag'
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -6,11 +7,31 @@ class MyDocument extends Document {
     return { ...initialProps }
   }
 
-  
+
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          {/* Global Site Tag (gtag.js) - Google Analytics  */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+              'anonymize_ip': true
+            });
+          `,
+            }}
+          />
+        </Head>
         <body className="light-theme">
           <Main />
           <NextScript />
