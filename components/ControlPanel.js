c239b6fe83e224a63ff62f0b2037ac2cde3e154e
tiacop <thomas.iacopino@eurac.edu>
2022-01-03 09:39:44 +0100

    add ticks with datalist standard html attribute

--- components/ControlPanel.js
+++ components/ControlPanel.js
@@ -1,51 +1,66 @@
-import * as React from 'react';
-import { format } from 'date-format-parse';
+import * as React from 'react'
+import { format } from 'date-format-parse'
+
 
 function ControlPanel(props) {
 
-  const {day, metadata, firstDay, lastDay} = props;
+  const { day, metadata, firstDay, lastDay } = props;
   const timestamp = format(day, 'X');
   const dayFromTimestamp = timestamp / 60 / 60 / 24
   const firstDayTimestamp = format(firstDay, 'X') / 60 / 60 / 24
   const lastDayTimestamp = format(lastDay, 'X') / 60 / 60 / 24
+
+  let rows = [];
+  for (let i = firstDayTimestamp; i <= lastDayTimestamp; i++) {
+    rows.push(<option key={i} value={i}></option>);
+  }
 
   return (
     <div className="controlpanel">
       <h2>{metadata?.short_name} - {metadata?.long_name}</h2>
       <h1>{day}</h1>
       <div key={'day'} className="timerangeSlider">
-        {dayFromTimestamp > firstDayTimestamp 
+
+
+
+        {dayFromTimestamp > firstDayTimestamp
           ? (
-              <button
-                title="prev"
-                type="submit"
-                value={dayFromTimestamp - 1}
-                onClick={evt => props.onChange(evt.target.value)}>
-                &lt;
-              </button>
-            )
+            <button
+              title="prev"
+              type="submit"
+              value={dayFromTimestamp - 1}
+              onClick={evt => props.onChange(evt.target.value)}>
+              &lt;
+            </button>
+          )
           : <button disabled={true}>&lt;</button>
         }
-      
-        <input
-        type="range"
-        value={dayFromTimestamp}
-        min={firstDayTimestamp}
-        max={lastDayTimestamp}
-        step={1}
-        onChange={evt => props.onChange(evt.target.value)}
-        />
 
-        {dayFromTimestamp < lastDayTimestamp 
+
+
+        <input
+          type="range"
+          list="tickmarks"
+          value={dayFromTimestamp}
+          min={firstDayTimestamp}
+          max={lastDayTimestamp}
+          step={1}
+          onChange={evt => props.onChange(evt.target.value)}
+        />
+        <datalist id="tickmarks">
+          {rows}
+        </datalist>
+
+        {dayFromTimestamp < lastDayTimestamp
           ? (
-              <button
-                title="next"
-                type="submit"
-                value={dayFromTimestamp + 1}
-                onClick={evt => props.onChange(evt.target.value)}>
-                  &gt;
-              </button>
-            )
+            <button
+              title="next"
+              type="submit"
+              value={dayFromTimestamp + 1}
+              onClick={evt => props.onChange(evt.target.value)}>
+              &gt;
+            </button>
+          )
           : <button disabled={true}>&gt;</button>
         }
       </div>
