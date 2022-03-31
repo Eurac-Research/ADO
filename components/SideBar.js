65c935b516c1fdaa9bd3a3fc63387583cc4d6c40
tiacop <thomas.iacopino@eurac.edu>
2022-03-31 18:25:33 +0200

    add initial hydro route

--- components/SideBar.js
+++ components/SideBar.js
@@ -7,19 +7,24 @@ function SideBar() {
 
   return (
     <div className="sideBar">
-      <div className={router.pathname !== "/impacts" ? "sideBarItem active" : "sideBarItem"}>
+      <div className={(router.pathname !== "/impacts" && router.pathname !== "/impacts-nuts2" && router.asPath != "/hydro/cdi") ? "sideBarItem active" : "sideBarItem"}>
         <Link href="/">
           <a>Indicies</a>
         </Link>
       </div>
-      <div className={router.pathname == "/impacts" ? "sideBarItem active" : "sideBarItem"}>
+      <div className={router.pathname === "/impacts" ? "sideBarItem active" : "sideBarItem"}>
         <Link href="/impacts">
           <a>Impacts (nuts3)</a>
         </Link>
       </div>
-      <div className={router.pathname == "/impacts-nuts2" ? "sideBarItem active" : "sideBarItem"}>
+      <div className={router.pathname === "/impacts-nuts2" ? "sideBarItem active" : "sideBarItem"}>
         <Link href="/impacts-nuts2">
           <a>Impacts (nuts2)</a>
+        </Link>
+      </div>
+      <div className={router.asPath == "/hydro/cdi" ? "sideBarItem active" : "sideBarItem"}>
+        <Link href="/hydro/cdi">
+          <a>Hydro</a>
         </Link>
       </div>
     </div>
